2cb92da990e2f3fcd7985a9d1fd77fd507632367
Thiago Cardoso <intelxd@gmail.com>
2021-02-21 21:28:23 -0300

    Refactor file read using pipe operator

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -1,7 +1,8 @@
 defmodule Fizzbuzz do
   def build(filename) do
-    file = File.read(filename)
-    handle_file_read(file)
+    filename
+    |> File.read()
+    |>handle_file_read()
   end
 
   def handle_file_read({:ok, result}), do: result
