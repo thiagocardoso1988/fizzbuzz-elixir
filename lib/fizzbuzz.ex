a61c706aa71c764695c2e3fc6fa6d03dd74db3d5
Thiago Cardoso <intelxd@gmail.com>
2021-02-21 21:38:32 -0300

    Converting string to an integer list with map and anonymous function

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -5,6 +5,9 @@ defmodule Fizzbuzz do
     |>handle_file_read()
   end
 
-  def handle_file_read({:ok, result}), do: result
+  def handle_file_read({:ok, result}) do
+    list = String.split(result, ",")
+    Enum.map(list, fn number -> String.to_integer(number) end)
+  end
   def handle_file_read({:error, reason}), do: reason
 end
