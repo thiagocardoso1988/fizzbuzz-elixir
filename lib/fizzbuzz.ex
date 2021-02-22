36f45ecadcd2ea1e2c15f8e68c4da44423276f2e
Thiago Cardoso <intelxd@gmail.com>
2021-02-21 22:05:57 -0300

    Includes guards to evaluate fizz or buzz results

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -5,10 +5,24 @@ defmodule Fizzbuzz do
     |>handle_file_read()
   end
 
-  def handle_file_read({:ok, result}) do
-    result
-    |> String.split(",")
-    |> Enum.map(&String.to_integer/1)
+  defp handle_file_read({:ok, result}) do
+    result =
+      result
+      |> String.split(",")
+      |> Enum.map(&convert_and_evaluate_numbers/1)
+
+    {:ok, result}
   end
-  def handle_file_read({:error, reason}), do: reason
+  defp handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
+
+  defp convert_and_evaluate_numbers(element) do
+    element
+    |> String.to_integer()
+    |> evaluate_number()
+  end
+
+  defp evaluate_number(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  defp evaluate_number(number) when rem(number, 3) == 0, do: :fizz
+  defp evaluate_number(number) when rem(number, 5) == 0, do: :buzz
+  defp evaluate_number(number), do: number
 end
